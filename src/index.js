802d237677272d8d03a33a46d6889b6dd9a6ed35
h3xk4 <michael@metaset.co>
2016-11-12 15:18:04 +1100

    Added 404 component.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { browserHistory, Router, Route, Link, History } from 'react-router'
+import { render } from 'react-dom'
+import { browserHistory, Router, Route, Link, History, IndexRoute } from 'react-router'
 
-// import NotFound from './ui/404'
 
-import HelloWorld from './ui/hello-world'
+import NotFound from './ui/404'
+//import HelloWorld from './ui/hello-world'
 
-ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('content')
-);
+
+/**
+ * Home page
+ */
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <h2>Home page yo</h2>
+    )
+  }
+}
+
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <h2>Hello there</h2>
+    )
+  }
+}
+
+
+/**
+ * Render the app
+ */
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={HomePage} />
+    <Route path="/hello" component={HelloWorld} />
+    <Route path="*" component={NotFound}/>
+  </Router>
+), document.getElementById('App'))
+
---
+++ src/ui/404/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+/**
+ * 404 not found
+ */
+
+class NotFound extends React.Component {
+
+  render() {
+    return (
+      <h1>404 not found</h1>
+    )
+  }
+}
+
+
+export default NotFound
